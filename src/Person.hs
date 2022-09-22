b5da018ec616a6a213bef9641adf22234f1f47de
ktf <not.discostoo@gmail.com>
2022-09-21 22:45:25 -0700

    Generate a stubbed list of YearAndMonths. Will use this to normalize a given user's list of monthly expenses, to fill in the "blanks".

--- src/Person.hs
+++ src/Person.hs
@@ -1,6 +1,6 @@
 module Person where
 
-import Expense (MonthlyTotal, YearAndMonth)
+import Expense (MonthlyTotal (MonthlyTotal), YearAndMonth, incrementMonth)
 
 data Person = Person
   { name :: String, -- TODO: text? Not important, just to differentiate
@@ -20,3 +20,24 @@ data MonthlyDebtSummary = MonthlyDebtSummary
 
 summarizeDebt :: Person -> Person -> [MonthlyDebtSummary]
 summarizeDebt p1 p2 = []
+
+-- Generate a "stub" of monthly totals, all zeroed, from the beginning month to
+-- ending month specified (inclusive).
+generateStubTotals :: YearAndMonth -> YearAndMonth -> [MonthlyTotal]
+generateStubTotals minMonth maxMonth = map totalFromTup totals
+  where
+    months = reverse (generateMonths maxMonth [minMonth]) -- Months are generated in descending order, so reverse it
+    totals = zip months (repeat 0)
+    totalFromTup (yearAndMonth, total) = MonthlyTotal yearAndMonth total
+
+-- Recursively generate a list of months (and years), given an upper bound and
+-- a starting list of months. Note that this list goes in descending order,
+-- mostly to make it easy to prepend the next value as we go.
+generateMonths :: YearAndMonth -> [YearAndMonth] -> [YearAndMonth]
+generateMonths upperBound months =
+  if latestMonth == upperBound -- then we're done
+    then months
+    else generateMonths upperBound (nextMonth : months)
+  where
+    latestMonth = head months
+    nextMonth = incrementMonth latestMonth
