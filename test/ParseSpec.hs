ef18decbca0fd74190b61bc16c9ff006e98c2c07
ktf <not.discostoo@gmail.com>
2022-09-29 21:21:35 -0700

    Test some parsing failure cases

--- test/ParseSpec.hs
+++ test/ParseSpec.hs
@@ -1,5 +1,6 @@
 module ParseSpec (spec) where
 
+import Error (Error (..), ErrorType (..))
 import Expense (Date (..), Expense (..))
 import Parse
 import Test.Hspec
@@ -20,7 +21,19 @@ spec = do
     it "parses a correctly-formed CSV line" $ do
       parseLine simpleConf simpleCsvLine `shouldBe` Right simpleLineResult
 
--- TODO: parse failure cases
+    it "returns a ParseError when parsing an invalid `amount`" $ do
+      parseLine badAmountConf simpleCsvLine `shouldSatisfy` isParseError
+
+    it "returns an InvalidInput error when given an out-of-bounds index" $ do
+      parseLine outOfBoundsConf simpleCsvLine `shouldSatisfy` isInvalidInput
+
+isParseError :: Either Error a -> Bool
+isParseError (Left (Error ParseError _)) = True
+isParseError _ = False
+
+isInvalidInput :: Either Error a -> Bool
+isInvalidInput (Left (Error InvalidInput _)) = True
+isInvalidInput _ = False
 
 simpleConf =
   ParseConf
@@ -36,6 +49,22 @@ headerConf =
       dateColNum = 0,
       amountColNum = 2,
       dataStartRow = 1 -- there's a header with col names before the data
+    }
+
+badAmountConf =
+  ParseConf
+    { colSep = ";",
+      dateColNum = 0,
+      amountColNum = 1, -- this ain't right! 1 is the description
+      dataStartRow = 0 -- data starts immediately
+    }
+
+outOfBoundsConf =
+  ParseConf
+    { colSep = ";",
+      dateColNum = 0,
+      amountColNum = 20, -- out of bounds!
+      dataStartRow = 0 -- data starts immediately
     }
 
 simpleCsv = "08/06/2022;BIG BURGERZ;-50.34\n08/31/2022;BIG BURGERZ;-93.21\n01/23/2023;STUFFZ;300"
